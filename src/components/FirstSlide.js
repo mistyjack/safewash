0a2f463c76d44c094a02ac11637cf047531f5572
mistyjack <oladepomustafa@gmail.com>
2021-04-29 16:52:57 +0100

    Removed styling.

--- src/components/FirstSlide.js
+++ src/components/FirstSlide.js
@@ -3,65 +3,20 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    container: {
-      minHeight: "670px",
-      [theme.breakpoints.up("md")]: {
-        minHeight: "100vh",
-        height: "772px"
-      }
-    },
-    stylishHeading: {
-      fontSize: 50,
-      [theme.breakpoints.down("sm")]: {
-        fontSize: 35,
-        lineHeight: "48px",
-        maxWidth: 600
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: 26,
-        lineHeight: "36px",
-        maxWidth: 330
-      },
-      lineHeight: "70.5px",
-      fontStyle: "normal",
-      fontFamily: "Cocon",
-      fontWeight: 700,
-      maxWidth: 701,
-      color: theme.palette.primary.main
-    },
-    textContent: {
-      position: "absolute",
-      top: "50%",
-      transform: "translateY(-50%)",
-      [theme.breakpoints.up("md")]: {
-        left: "10.1rem"
-      },
-      [theme.breakpoints.down("sm")]: {
-        left: "2rem"
-      },
-      [theme.breakpoints.down("xs")]: {
-        left: 0
-      },
-      zIndex: 10,
-      padding: theme.spacing(2)
-    }
-  })
-);
+const useStyles = makeStyles(theme => createStyles({}));
 
 const FirstSlide = () => {
   const classes = useStyles();
 
   return (
-    <section className={classes.container}>
+    <section>
       <CssBaseline />
 
-      <div className={classes.textContent}>
-        <Typography className={classes.stylishHeading} variant="h1" gutterBottom>
+      <div>
+        <Typography variant="h1" gutterBottom>
           A Liquid Laundry Detergent Specifically Formulated for
         </Typography>
-        <Typography className={classes.blockHeading} variant="h1" gutterBottom>
+        <Typography variant="h1" gutterBottom>
           Babies and Children Clothings
         </Typography>
         <Button className="btn" variant="contained" color="secondary" disableElevation>
